b87b19a0bab6e1331f2da878b39501b9248e77cf
Shiland <shilandi@hpdnyc.org>
2017-12-21 10:34:13 -0500

    delete scrap.py

--- app/scrap.py
+++
@@ -1,9 +0,0 @@
-from app.models import History, Product, db, Server, Updates
-from sqlalchemy import func, desc, and_
-
-server_id = 1
-
-checked_out = History.time_in_none(server_id)
-
-for c in checked_out:
-    print(c.time_out.year)
